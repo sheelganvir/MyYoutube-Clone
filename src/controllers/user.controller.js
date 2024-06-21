75d496461664f15de53d5ecae1461717fa2e4c04
Sheel Ganvir <sheelganvir2805@gmail.com>
2024-06-21 21:57:25 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -100,7 +100,7 @@ const loginUser = asyncHandler(async (req,res) => {
     
     const {email, username, password} = req.body
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400, "username or email is required")
     }
 
@@ -142,6 +142,7 @@ const loginUser = asyncHandler(async (req,res) => {
         )
     )
 })
+
 
 const logoutUser = asyncHandler(async(req,res) => {
     await User.findByIdAndUpdate(
